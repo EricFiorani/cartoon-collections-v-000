5ef1a9cb8cfb1c080908cc35b858024aa4aa3ec3
Eric Fiorani <efiorani305@gmail.com>
2018-04-24 17:10:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_plane(veggies)# code an argument here
-  veggies.map {|x| return x.capitalize + "!"}
+  veggies.map {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
