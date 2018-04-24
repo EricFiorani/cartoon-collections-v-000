b97bd60a02e7269c11220b5f8dde20401f738a1f
Eric Fiorani <efiorani305@gmail.com>
2018-04-24 17:33:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(veggies)# code an argument here
 end
 
 def long_planeteer_calls(calls_long)# code an argument here
-  calls_long.map.min(4){|x| x.length > 4}# Your code here
+  calls_long.map.min(4)# Your code here
 end
 
 def find_the_cheese# code an argument here
