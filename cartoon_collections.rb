f6516673281be13be082a31efbb1eba6af60c26d
Eric Fiorani <efiorani305@gmail.com>
2018-04-24 17:39:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,8 @@ def long_planeteer_calls(calls_long)# code an argument here
 end
 
 def find_the_cheese(cheddar_cheese)
+  if cheddar_cheese.map.include?("cheddar")
+    return cheddar_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
