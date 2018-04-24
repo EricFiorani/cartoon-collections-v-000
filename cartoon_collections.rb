1912dcd38ef977b10aea483afefae76bf60ee983
Eric Fiorani <efiorani305@gmail.com>
2018-04-24 17:16:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_plane(array)# code an argument here
+def summon_captain_plane(veggies)# code an argument here
   array.map{|x| x.capitalize + "!"}
 end
 
